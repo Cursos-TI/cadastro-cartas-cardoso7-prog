93262d3f82d1cc9b7255af5d5556c7ca181a51d2
cardoso7-prog <yasmiim.martiins2008@gmail.com>
2026-04-05 22:50:33 +0000

    acrescentei printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,7 +8,7 @@ int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
   // Área para entrada de dados
-
+printf("Ola mundo\n");
   // Área para exibição dos dados da cidade
 
 return 0;
